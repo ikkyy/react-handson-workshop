90a8fe367ca8ebbbbfe564c391d19cb954fec557
Ikkyy <msndoikky@hotmail.com>
2019-08-07 21:46:01 -0300

    Dias 2 e 3

---
+++ minhas-series/src/Series.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const Series = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios
+            .get('/api/series')
+            .then(res => {
+                setData(res.data.data)
+            })
+    }, [])
+
+    const deleteSerie = id => {
+        axios
+            .delete('/api/series/' + id)
+            .then(res => {
+                const filtro = data.filter(item => item.id !== id)
+                setData(filtro)
+            })
+    }
+
+    const renderizaLinha = record => {
+        return (
+            <tr key={record.id}>
+                <th scope="row">{record.id}</th>
+                <td>{record.name}</td>
+                <td>
+                    <button className="btn btn-danger mr-2" onClick={() => deleteSerie(record.id)}>Remover</button>
+                    <Link to={'/series/' + record.id} className="btn btn-info">Info</Link>
+                    </td>
+            </tr>
+        )
+    }
+
+    if (data.length === 0) {
+        return (
+            <div className='container'>
+                <h1>Séries</h1>
+                <div><Link className="btn btn-primary mb-2" to='/series/novo'>Nova Série</Link></div>
+                <div className="alert alert-warning" role="alert">
+                    Você não possui séries criadas.
+                    </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='container'>
+            <h1>Série</h1>
+            <div><Link className="btn btn-primary mb-2" to='/series/novo'>Nova Série</Link></div>
+            <table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(renderizaLinha)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Series
